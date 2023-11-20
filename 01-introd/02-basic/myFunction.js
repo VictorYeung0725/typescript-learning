8a50e1d3ccd90fc99c7310650fcc2effbaf3717e
VictorYeung0725 <96713542+VictorYeung0725@users.noreply.github.com>
2023-11-20 16:16:03 +0800

    learning type in function

---
+++ 01-introd/02-basic/myFunction.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function addTwo(num) {
+    // num.toUppercase();
+    // return num + '2';
+    return num + 2;
+    // return 'hello world';
+}
+addTwo(5);
+console.log(addTwo(5));
+function getUpper(val) {
+    // return val.toUppercase();
+}
+// getUpper(5);
+// console.log(getUpper(4));
+function signupUser(name, email, password) { }
+signupUser('1', '2', '3');
+var loginUser = function (name, password, isLoggedIn) {
+    if (isLoggedIn === void 0) { isLoggedIn = false; }
+};
+loginUser('1', 'h@h');
+function getValue(myVal) {
+    if (myVal > 5) {
+        return true;
+    }
+    return '200 Ok';
+}
+var getHello = function (s) {
+    return 'helloworld';
+};
